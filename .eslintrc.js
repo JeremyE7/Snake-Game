1bb99a0f7d211964f4874034ee307830c9eb2e0e
Jeremy Encalada <encaladajeremy@gmail.com>
2024-07-01 14:15:36 +0000

    add eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    extends: 'standard',
+    rules: {
+      // Aquí puedes añadir o sobrescribir reglas específicas
+      // Por ejemplo:
+      // "no-console": "off",
+      // "indent": ["error", 2],
+    }
+  }
+  
