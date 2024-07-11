2124f5a330b90a7febba4f8cefa48ba3fc3b179a
Jeremy Encalada <encaladajeremy@gmail.com>
2024-07-11 08:38:13 -0500

    Update and rename .eslintrc.js to .eslintrc.json

--- .eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  extends: 'standard',
-  rules: {
-    // Aquí puedes añadir o sobrescribir reglas específicas
-    // Por ejemplo:
-    // "no-console": "off",
-    // "indent": ["error", 2],
-  }
-}
